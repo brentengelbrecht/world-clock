eacfc9a3e8076660d14c7eba5d510d6d3868038b
Brent Engelbrecht <brent@zooloo.co.za>
2022-12-14 00:18:51 +0000

    Additional visual design of settings dialog

--- WorldClock/WorldClock/SettingsForm.Designer.cs
+++ WorldClock/WorldClock/SettingsForm.Designer.cs
@@ -39,25 +39,32 @@ namespace WorldClock
             this.TextBoxFilter = new System.Windows.Forms.TextBox();
             this.ListBoxPlaces = new System.Windows.Forms.ListBox();
             this.ListBoxSelected = new System.Windows.Forms.ListBox();
-            this.TabPageOther = new System.Windows.Forms.TabPage();
+            this.TabPageAppearance = new System.Windows.Forms.TabPage();
             this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.CheckBoxIncludeSeconds = new System.Windows.Forms.CheckBox();
             this.CheckBox24Hour = new System.Windows.Forms.CheckBox();
             this.ButtonOk = new System.Windows.Forms.Button();
             this.ButtonCancel = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.CheckBoxLaunch = new System.Windows.Forms.CheckBox();
+            this.CheckBoxMinimize = new System.Windows.Forms.CheckBox();
+            this.CheckBoxStatusArea = new System.Windows.Forms.CheckBox();
             this.TabControl.SuspendLayout();
             this.TabPageTimezones.SuspendLayout();
-            this.TabPageOther.SuspendLayout();
+            this.TabPageAppearance.SuspendLayout();
             this.GroupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // TabControl
             // 
             this.TabControl.Controls.Add(this.TabPageTimezones);
-            this.TabControl.Controls.Add(this.TabPageOther);
-            this.TabControl.Location = new System.Drawing.Point(6, 6);
+            this.TabControl.Controls.Add(this.TabPageAppearance);
+            this.TabControl.Location = new System.Drawing.Point(8, 7);
+            this.TabControl.Margin = new System.Windows.Forms.Padding(4);
             this.TabControl.Name = "TabControl";
             this.TabControl.SelectedIndex = 0;
-            this.TabControl.Size = new System.Drawing.Size(643, 293);
+            this.TabControl.Size = new System.Drawing.Size(857, 361);
             this.TabControl.TabIndex = 0;
             // 
             // TabPageTimezones
@@ -70,10 +77,11 @@ namespace WorldClock
             this.TabPageTimezones.Controls.Add(this.TextBoxFilter);
             this.TabPageTimezones.Controls.Add(this.ListBoxPlaces);
             this.TabPageTimezones.Controls.Add(this.ListBoxSelected);
-            this.TabPageTimezones.Location = new System.Drawing.Point(4, 22);
+            this.TabPageTimezones.Location = new System.Drawing.Point(4, 25);
+            this.TabPageTimezones.Margin = new System.Windows.Forms.Padding(4);
             this.TabPageTimezones.Name = "TabPageTimezones";
-            this.TabPageTimezones.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
-            this.TabPageTimezones.Size = new System.Drawing.Size(635, 267);
+            this.TabPageTimezones.Padding = new System.Windows.Forms.Padding(4);
+            this.TabPageTimezones.Size = new System.Drawing.Size(849, 332);
             this.TabPageTimezones.TabIndex = 0;
             this.TabPageTimezones.Text = "Timezones";
             this.TabPageTimezones.UseVisualStyleBackColor = true;
@@ -81,17 +89,19 @@ namespace WorldClock
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(50, 32);
+            this.label1.Location = new System.Drawing.Point(67, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.Size = new System.Drawing.Size(39, 17);
             this.label1.TabIndex = 7;
             this.label1.Text = "Filter";
             // 
             // ButtonDown
             // 
-            this.ButtonDown.Location = new System.Drawing.Point(543, 128);
+            this.ButtonDown.Location = new System.Drawing.Point(724, 158);
+            this.ButtonDown.Margin = new System.Windows.Forms.Padding(4);
             this.ButtonDown.Name = "ButtonDown";
-            this.ButtonDown.Size = new System.Drawing.Size(30, 23);
+            this.ButtonDown.Size = new System.Drawing.Size(40, 28);
             this.ButtonDown.TabIndex = 6;
             this.ButtonDown.Text = "down";
             this.ButtonDown.UseVisualStyleBackColor = true;
@@ -99,9 +109,10 @@ namespace WorldClock
             // 
             // ButtonUp
             // 
-            this.ButtonUp.Location = new System.Drawing.Point(543, 99);
+            this.ButtonUp.Location = new System.Drawing.Point(724, 122);
+            this.ButtonUp.Margin = new System.Windows.Forms.Padding(4);
             this.ButtonUp.Name = "ButtonUp";
-            this.ButtonUp.Size = new System.Drawing.Size(30, 23);
+            this.ButtonUp.Size = new System.Drawing.Size(40, 28);
             this.ButtonUp.TabIndex = 5;
             this.ButtonUp.Text = "up";
             this.ButtonUp.UseVisualStyleBackColor = true;
@@ -109,9 +120,10 @@ namespace WorldClock
             // 
             // ButtonOut
             // 
-            this.ButtonOut.Location = new System.Drawing.Point(288, 128);
+            this.ButtonOut.Location = new System.Drawing.Point(384, 158);
+            this.ButtonOut.Margin = new System.Windows.Forms.Padding(4);
             this.ButtonOut.Name = "ButtonOut";
-            this.ButtonOut.Size = new System.Drawing.Size(30, 23);
+            this.ButtonOut.Size = new System.Drawing.Size(40, 28);
             this.ButtonOut.TabIndex = 3;
             this.ButtonOut.Text = "out";
             this.ButtonOut.UseVisualStyleBackColor = true;
@@ -119,9 +131,10 @@ namespace WorldClock
             // 
             // ButtonIn
             // 
-            this.ButtonIn.Location = new System.Drawing.Point(288, 99);
+            this.ButtonIn.Location = new System.Drawing.Point(384, 122);
+            this.ButtonIn.Margin = new System.Windows.Forms.Padding(4);
             this.ButtonIn.Name = "ButtonIn";
-            this.ButtonIn.Size = new System.Drawing.Size(30, 23);
+            this.ButtonIn.Size = new System.Drawing.Size(40, 28);
             this.ButtonIn.TabIndex = 2;
             this.ButtonIn.Text = "in";
             this.ButtonIn.UseVisualStyleBackColor = true;
@@ -129,67 +142,92 @@ namespace WorldClock
             // 
             // TextBoxFilter
             // 
-            this.TextBoxFilter.Location = new System.Drawing.Point(84, 29);
+            this.TextBoxFilter.Location = new System.Drawing.Point(112, 36);
+            this.TextBoxFilter.Margin = new System.Windows.Forms.Padding(4);
             this.TextBoxFilter.Name = "TextBoxFilter";
-            this.TextBoxFilter.Size = new System.Drawing.Size(182, 20);
+            this.TextBoxFilter.Size = new System.Drawing.Size(241, 22);
             this.TextBoxFilter.TabIndex = 0;
             this.TextBoxFilter.TextChanged += new System.EventHandler(this.TextBoxFilter_TextChanged);
             // 
             // ListBoxPlaces
             // 
-            this.ListBoxPlaces.Location = new System.Drawing.Point(82, 55);
+            this.ListBoxPlaces.ItemHeight = 16;
+            this.ListBoxPlaces.Location = new System.Drawing.Point(109, 68);
+            this.ListBoxPlaces.Margin = new System.Windows.Forms.Padding(4);
             this.ListBoxPlaces.Name = "ListBoxPlaces";
             this.ListBoxPlaces.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
-            this.ListBoxPlaces.Size = new System.Drawing.Size(184, 160);
+            this.ListBoxPlaces.Size = new System.Drawing.Size(244, 196);
             this.ListBoxPlaces.Sorted = true;
             this.ListBoxPlaces.TabIndex = 1;
             // 
             // ListBoxSelected
             // 
             this.ListBoxSelected.FormattingEnabled = true;
-            this.ListBoxSelected.Location = new System.Drawing.Point(340, 55);
+            this.ListBoxSelected.ItemHeight = 16;
+            this.ListBoxSelected.Location = new System.Drawing.Point(453, 68);
+            this.ListBoxSelected.Margin = new System.Windows.Forms.Padding(4);
             this.ListBoxSelected.Name = "ListBoxSelected";
-            this.ListBoxSelected.Size = new System.Drawing.Size(184, 160);
+            this.ListBoxSelected.Size = new System.Drawing.Size(244, 196);
             this.ListBoxSelected.TabIndex = 4;
             // 
-            // TabPageOther
+            // TabPageAppearance
             // 
-            this.TabPageOther.Controls.Add(this.GroupBox1);
-            this.TabPageOther.Location = new System.Drawing.Point(4, 22);
-            this.TabPageOther.Name = "TabPageOther";
-            this.TabPageOther.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
-            this.TabPageOther.Size = new System.Drawing.Size(635, 267);
-            this.TabPageOther.TabIndex = 1;
-            this.TabPageOther.Text = "Other";
-            this.TabPageOther.UseVisualStyleBackColor = true;
+            this.TabPageAppearance.Controls.Add(this.groupBox2);
+            this.TabPageAppearance.Controls.Add(this.GroupBox1);
+            this.TabPageAppearance.Location = new System.Drawing.Point(4, 25);
+            this.TabPageAppearance.Margin = new System.Windows.Forms.Padding(4);
+            this.TabPageAppearance.Name = "TabPageAppearance";
+            this.TabPageAppearance.Padding = new System.Windows.Forms.Padding(4);
+            this.TabPageAppearance.Size = new System.Drawing.Size(849, 332);
+            this.TabPageAppearance.TabIndex = 1;
+            this.TabPageAppearance.Text = "Appearance";
+            this.TabPageAppearance.UseVisualStyleBackColor = true;
             // 
             // GroupBox1
             // 
+            this.GroupBox1.Controls.Add(this.CheckBoxIncludeSeconds);
             this.GroupBox1.Controls.Add(this.CheckBox24Hour);
-            this.GroupBox1.Location = new System.Drawing.Point(23, 20);
+            this.GroupBox1.Location = new System.Drawing.Point(44, 25);
+            this.GroupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.GroupBox1.Name = "GroupBox1";
-            this.GroupBox1.Size = new System.Drawing.Size(294, 225);
+            this.GroupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.GroupBox1.Size = new System.Drawing.Size(347, 277);
             this.GroupBox1.TabIndex = 0;
             this.GroupBox1.TabStop = false;
             this.GroupBox1.Text = "Display";
+            // 
+            // CheckBoxIncludeSeconds
+            // 
+            this.CheckBoxIncludeSeconds.AutoSize = true;
+            this.CheckBoxIncludeSeconds.Checked = true;
+            this.CheckBoxIncludeSeconds.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CheckBoxIncludeSeconds.Location = new System.Drawing.Point(33, 65);
+            this.CheckBoxIncludeSeconds.Margin = new System.Windows.Forms.Padding(4);
+            this.CheckBoxIncludeSeconds.Name = "CheckBoxIncludeSeconds";
+            this.CheckBoxIncludeSeconds.Size = new System.Drawing.Size(132, 21);
+            this.CheckBoxIncludeSeconds.TabIndex = 1;
+            this.CheckBoxIncludeSeconds.Text = "Include seconds";
+            this.CheckBoxIncludeSeconds.UseVisualStyleBackColor = true;
             // 
             // CheckBox24Hour
             // 
             this.CheckBox24Hour.AutoSize = true;
             this.CheckBox24Hour.Checked = true;
             this.CheckBox24Hour.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.CheckBox24Hour.Location = new System.Drawing.Point(25, 29);
+            this.CheckBox24Hour.Location = new System.Drawing.Point(33, 36);
+            this.CheckBox24Hour.Margin = new System.Windows.Forms.Padding(4);
             this.CheckBox24Hour.Name = "CheckBox24Hour";
-            this.CheckBox24Hour.Size = new System.Drawing.Size(91, 17);
+            this.CheckBox24Hour.Size = new System.Drawing.Size(116, 21);
             this.CheckBox24Hour.TabIndex = 0;
             this.CheckBox24Hour.Text = "24-hour clock";
             this.CheckBox24Hour.UseVisualStyleBackColor = true;
             // 
             // ButtonOk
             // 
-            this.ButtonOk.Location = new System.Drawing.Point(508, 322);
+            this.ButtonOk.Location = new System.Drawing.Point(677, 396);
+            this.ButtonOk.Margin = new System.Windows.Forms.Padding(4);
             this.ButtonOk.Name = "ButtonOk";
-            this.ButtonOk.Size = new System.Drawing.Size(75, 23);
+            this.ButtonOk.Size = new System.Drawing.Size(100, 28);
             this.ButtonOk.TabIndex = 2;
             this.ButtonOk.Text = "O&k";
             this.ButtonOk.UseVisualStyleBackColor = true;
@@ -198,26 +236,75 @@ namespace WorldClock
             // ButtonCancel
             // 
             this.ButtonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.ButtonCancel.Location = new System.Drawing.Point(404, 322);
+            this.ButtonCancel.Location = new System.Drawing.Point(539, 396);
+            this.ButtonCancel.Margin = new System.Windows.Forms.Padding(4);
             this.ButtonCancel.Name = "ButtonCancel";
-            this.ButtonCancel.Size = new System.Drawing.Size(75, 23);
+            this.ButtonCancel.Size = new System.Drawing.Size(100, 28);
             this.ButtonCancel.TabIndex = 1;
             this.ButtonCancel.Text = "Cancel";
             this.ButtonCancel.UseVisualStyleBackColor = true;
             this.ButtonCancel.Click += new System.EventHandler(this.ButtonCancel_Click);
             // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.CheckBoxStatusArea);
+            this.groupBox2.Controls.Add(this.CheckBoxMinimize);
+            this.groupBox2.Controls.Add(this.CheckBoxLaunch);
+            this.groupBox2.Location = new System.Drawing.Point(439, 25);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(365, 277);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Program";
+            // 
+            // CheckBoxLaunch
+            // 
+            this.CheckBoxLaunch.AutoSize = true;
+            this.CheckBoxLaunch.Location = new System.Drawing.Point(33, 36);
+            this.CheckBoxLaunch.Margin = new System.Windows.Forms.Padding(4);
+            this.CheckBoxLaunch.Name = "CheckBoxLaunch";
+            this.CheckBoxLaunch.Size = new System.Drawing.Size(185, 21);
+            this.CheckBoxLaunch.TabIndex = 0;
+            this.CheckBoxLaunch.Text = "Launch at Windows start";
+            this.CheckBoxLaunch.UseVisualStyleBackColor = true;
+            // 
+            // CheckBoxMinimize
+            // 
+            this.CheckBoxMinimize.AutoSize = true;
+            this.CheckBoxMinimize.Location = new System.Drawing.Point(33, 65);
+            this.CheckBoxMinimize.Margin = new System.Windows.Forms.Padding(4);
+            this.CheckBoxMinimize.Name = "CheckBoxMinimize";
+            this.CheckBoxMinimize.Size = new System.Drawing.Size(143, 21);
+            this.CheckBoxMinimize.TabIndex = 1;
+            this.CheckBoxMinimize.Text = "Minimize on Close";
+            this.CheckBoxMinimize.UseVisualStyleBackColor = true;
+            // 
+            // CheckBoxStatusArea
+            // 
+            this.CheckBoxStatusArea.AutoSize = true;
+            this.CheckBoxStatusArea.Location = new System.Drawing.Point(33, 94);
+            this.CheckBoxStatusArea.Margin = new System.Windows.Forms.Padding(4);
+            this.CheckBoxStatusArea.Name = "CheckBoxStatusArea";
+            this.CheckBoxStatusArea.Size = new System.Drawing.Size(223, 26);
+            this.CheckBoxStatusArea.TabIndex = 2;
+            this.CheckBoxStatusArea.Text = "Minimize to Status Area";
+            this.CheckBoxStatusArea.UseVisualStyleBackColor = true;
+            // 
             // SettingsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.ButtonCancel;
-            this.ClientSize = new System.Drawing.Size(656, 367);
+            this.ClientSize = new System.Drawing.Size(875, 452);
             this.Controls.Add(this.ButtonCancel);
             this.Controls.Add(this.ButtonOk);
             this.Controls.Add(this.TabControl);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Location = new System.Drawing.Point(0, 10);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "SettingsForm";
@@ -226,9 +313,11 @@ namespace WorldClock
             this.TabControl.ResumeLayout(false);
             this.TabPageTimezones.ResumeLayout(false);
             this.TabPageTimezones.PerformLayout();
-            this.TabPageOther.ResumeLayout(false);
+            this.TabPageAppearance.ResumeLayout(false);
             this.GroupBox1.ResumeLayout(false);
             this.GroupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -247,8 +336,13 @@ namespace WorldClock
         private System.Windows.Forms.ListBox ListBoxPlaces;
         private System.Windows.Forms.ListBox ListBoxSelected;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TabPage TabPageOther;
+        private System.Windows.Forms.TabPage TabPageAppearance;
         private System.Windows.Forms.GroupBox GroupBox1;
         private System.Windows.Forms.CheckBox CheckBox24Hour;
+        private System.Windows.Forms.CheckBox CheckBoxIncludeSeconds;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox CheckBoxLaunch;
+        private System.Windows.Forms.CheckBox CheckBoxStatusArea;
+        private System.Windows.Forms.CheckBox CheckBoxMinimize;
     }
 }
